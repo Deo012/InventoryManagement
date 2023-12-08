3351c2050176220f189671d4d9b5979ced69a1bb
Odjoumani Tayo Déo-Gracias Giscar-Bonaventure <deogracias.odjoumani@gmail.com>
2023-12-08 17:11:32 -0500

    The form manageCustomers is added. Button add, edit, delete work

---
+++ InventoryManagement/ManageCustomers.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace InventoryManagement
+{
+    partial class ManageCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CustomerPhoneBox = new InventoryManagement.CustomTextBox1();
+            this.CustomerNameBox = new InventoryManagement.CustomTextBox1();
+            this.CustomerIdBox = new InventoryManagement.CustomTextBox1();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CustomersGV = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(936, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(903, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(324, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Manage Customers";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(193, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(545, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INVENTORY MANAGEMENT SYSTEM";
+            // 
+            // CustomerPhoneBox
+            // 
+            this.CustomerPhoneBox.BackColor = System.Drawing.SystemColors.Window;
+            this.CustomerPhoneBox.BorderColor = System.Drawing.Color.Gray;
+            this.CustomerPhoneBox.BorderFocusColor = System.Drawing.Color.LightBlue;
+            this.CustomerPhoneBox.BorderHoverColor = System.Drawing.Color.LightCyan;
+            this.CustomerPhoneBox.BorderSize = 2;
+            this.CustomerPhoneBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerPhoneBox.ForeColor = System.Drawing.Color.DimGray;
+            this.CustomerPhoneBox.Location = new System.Drawing.Point(58, 222);
+            this.CustomerPhoneBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CustomerPhoneBox.Multiline = false;
+            this.CustomerPhoneBox.Name = "CustomerPhoneBox";
+            this.CustomerPhoneBox.Padding = new System.Windows.Forms.Padding(7);
+            this.CustomerPhoneBox.PasswordChar = false;
+            this.CustomerPhoneBox.Size = new System.Drawing.Size(164, 31);
+            this.CustomerPhoneBox.TabIndex = 6;
+            this.CustomerPhoneBox.Texts = "CustomerPhone";
+            this.CustomerPhoneBox.UnderlineStyle = true;
+            // 
+            // CustomerNameBox
+            // 
+            this.CustomerNameBox.BackColor = System.Drawing.SystemColors.Window;
+            this.CustomerNameBox.BorderColor = System.Drawing.Color.Gray;
+            this.CustomerNameBox.BorderFocusColor = System.Drawing.Color.LightBlue;
+            this.CustomerNameBox.BorderHoverColor = System.Drawing.Color.LightCyan;
+            this.CustomerNameBox.BorderSize = 2;
+            this.CustomerNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerNameBox.ForeColor = System.Drawing.Color.DimGray;
+            this.CustomerNameBox.Location = new System.Drawing.Point(58, 183);
+            this.CustomerNameBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CustomerNameBox.Multiline = false;
+            this.CustomerNameBox.Name = "CustomerNameBox";
+            this.CustomerNameBox.Padding = new System.Windows.Forms.Padding(7);
+            this.CustomerNameBox.PasswordChar = false;
+            this.CustomerNameBox.Size = new System.Drawing.Size(164, 31);
+            this.CustomerNameBox.TabIndex = 5;
+            this.CustomerNameBox.Texts = "CustomerName";
+            this.CustomerNameBox.UnderlineStyle = true;
+            // 
+            // CustomerIdBox
+            // 
+            this.CustomerIdBox.BackColor = System.Drawing.SystemColors.Window;
+            this.CustomerIdBox.BorderColor = System.Drawing.Color.Gray;
+            this.CustomerIdBox.BorderFocusColor = System.Drawing.Color.LightBlue;
+            this.CustomerIdBox.BorderHoverColor = System.Drawing.Color.LightCyan;
+            this.CustomerIdBox.BorderSize = 2;
+            this.CustomerIdBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerIdBox.ForeColor = System.Drawing.Color.DimGray;
+            this.CustomerIdBox.Location = new System.Drawing.Point(58, 144);
+            this.CustomerIdBox.Margin = new System.Windows.Forms.Padding(4);
+            this.CustomerIdBox.Multiline = false;
+            this.CustomerIdBox.Name = "CustomerIdBox";
+            this.CustomerIdBox.Padding = new System.Windows.Forms.Padding(7);
+            this.CustomerIdBox.PasswordChar = false;
+            this.CustomerIdBox.Size = new System.Drawing.Size(164, 31);
+            this.CustomerIdBox.TabIndex = 4;
+            this.CustomerIdBox.Texts = "CustomerId";
+            this.CustomerIdBox.UnderlineStyle = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label4.Location = new System.Drawing.Point(493, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CUSTOMERS LIST";
+            // 
+            // CustomersGV
+            // 
+            this.CustomersGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CustomersGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.ForestGreen;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.ForestGreen;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CustomersGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.CustomersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CustomersGV.DefaultCellStyle = dataGridViewCellStyle8;
+            this.CustomersGV.Location = new System.Drawing.Point(364, 183);
+            this.CustomersGV.MultiSelect = false;
+            this.CustomersGV.Name = "CustomersGV";
+            this.CustomersGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.CustomersGV.Size = new System.Drawing.Size(503, 314);
+            this.CustomersGV.TabIndex = 12;
+            this.CustomersGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CustomersGV_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.ForestGreen;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(102, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Home";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.ForestGreen;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(183, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(102, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 532);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(936, 10);
+            this.panel2.TabIndex = 18;
+            // 
+            // ManageCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 542);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CustomersGV);
+            this.Controls.Add(this.CustomerPhoneBox);
+            this.Controls.Add(this.CustomerNameBox);
+            this.Controls.Add(this.CustomerIdBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManageCustomers";
+            this.Text = "ManageCustomers";
+            this.Load += new System.EventHandler(this.ManageCustomers_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private CustomTextBox1 CustomerPhoneBox;
+        private CustomTextBox1 CustomerNameBox;
+        private CustomTextBox1 CustomerIdBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView CustomersGV;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ InventoryManagement/ManageCustomers.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement
+{
+    public partial class ManageCustomers : Form
+    {
+        SqlConnection databaseConnect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Odjoijmani Deo\Documents\Inventorydb.mdf"";Integrated Security=True;Connect Timeout=30");
+
+        public ManageCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                databaseConnect.Open();
+                SqlCommand cmd = new SqlCommand("insert into CustomerTable values('"+CustomerIdBox.Texts+ "', '"+CustomerNameBox.Texts+ "', '"+CustomerPhoneBox.Texts+"')", databaseConnect);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("User Successfully added");
+                databaseConnect.Close();
+                populate();
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+        public void populate()
+        {
+            try
+            {
+                databaseConnect.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("Select * from CustomerTable", databaseConnect);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "CustomerTable");
+                CustomersGV.DataSource = ds.Tables[0];
+                databaseConnect.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Il y a eu une erreur {e}");
+            }
+        }
+
+        private void CustomersGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CustomerIdBox.Texts = CustomersGV.SelectedRows[0].Cells[0].Value.ToString();
+            CustomerNameBox.Texts = CustomersGV.SelectedRows[0].Cells[1].Value.ToString();
+            CustomerPhoneBox.Texts = CustomersGV.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void ManageCustomers_Load(object sender, EventArgs e)
+        {
+            populate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                databaseConnect.Open();
+                SqlCommand updateCustomerTable = new SqlCommand("Update CustomerTable set CustomerId = '"+CustomerIdBox.Texts+ "', CustomerName = '"+CustomerNameBox.Texts+"' where CustomerPhone = '"+CustomerPhoneBox.Texts+"'", databaseConnect);
+                updateCustomerTable.ExecuteNonQuery();
+                MessageBox.Show("User Successfully Updated");
+                databaseConnect.Close();
+                populate();
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                databaseConnect.Open();
+                SqlCommand deleteCustomerTable = new SqlCommand("delete from CustomerTable where CustomerPhone = '" + CustomerPhoneBox.Texts + "'", databaseConnect);
+                deleteCustomerTable.ExecuteNonQuery();
+                MessageBox.Show("User Successfully Deleted");
+                databaseConnect.Close();
+                populate();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
